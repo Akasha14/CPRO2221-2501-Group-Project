836bf614e61a2ce23dddc63fa740d18e1e02f608
Ardan Kasha <ardan.kasha@rdpolytech.ca>
2024-11-25 00:26:07 -0700

    Added further overworld ability
    - Character movement
    -Start screen css

--- frontend/game_project_react/src/overworldComponents/GameCanvas.js
+++ frontend/game_project_react/src/overworldComponents/GameCanvas.js
@@ -4,7 +4,10 @@ import { resources } from "./GameResources.js";
 import { Sprite } from "./Sprite.js";
 import { Vector2 } from "./Vector2.js";
 import { GameLoop } from "./GameLoop.js";
-import { Input } from "./Input.js";
+import { Input, DOWN, UP, LEFT, RIGHT } from "./Input.js";
+import { gridCells, isSpaceFree } from "./helpers/grid.js";
+import { moveTowards } from "./helpers/moveTowards.js";
+import { walls } from "./walls/overworld.js";
 
 const GameCanvas = ({ characterData, onStartNewGame, onContinueGame }) => {
   // Create a ref for the canvas element
@@ -26,18 +29,57 @@ const GameCanvas = ({ characterData, onStartNewGame, onContinueGame }) => {
     hFrames: 3,
     vFrames: 8,
     frame: 1,
+    position: new Vector2(gridCells(6), gridCells(5)),
   });
+
+  const heroDestinationPosition = hero.position.duplicate();
 
   const shadow = new Sprite({
     resource: resources.images.shadow,
     frameSize: new Vector2(32, 32),
   });
 
-  const heroPos = new Vector2(16 * 6, 16 * 5);
   const input = new Input();
 
   const update = () => {
-    console.log(input.direction);
+    const distance = moveTowards(hero, heroDestinationPosition, 1);
+    const hasArrived = distance <= 1; // Allows wiggle room so character will finish moving to grid spot.
+    if (hasArrived) {
+      tryMove();
+    }
+  };
+
+  const tryMove = () => {
+    if (!input.direction) {
+      return;
+    }
+
+    let nextX = heroDestinationPosition.x;
+    let nextY = heroDestinationPosition.y;
+    const gridSize = 16;
+
+    if (input.direction === DOWN) {
+      nextY += gridSize;
+      hero.frame = 0;
+    }
+    if (input.direction === UP) {
+      nextY -= gridSize;
+      hero.frame = 6;
+    }
+    if (input.direction === LEFT) {
+      nextX -= gridSize;
+      hero.frame = 9;
+    }
+    if (input.direction === RIGHT) {
+      nextX += gridSize;
+      hero.frame = 3;
+    }
+
+    // Check for walls.
+    if (isSpaceFree(walls, nextX, nextY)) {
+      heroDestinationPosition.x = nextX;
+      heroDestinationPosition.y = nextY;
+    }
   };
 
   // Draw function
@@ -51,8 +93,8 @@ const GameCanvas = ({ characterData, onStartNewGame, onContinueGame }) => {
     groundSprite.drawImage(ctx, 0, 0);
 
     const heroOffset = new Vector2(-8, -21);
-    const heroPosX = heroPos.x + heroOffset.x;
-    const heroPosY = heroPos.y + heroOffset.y;
+    const heroPosX = hero.position.x + heroOffset.x;
+    const heroPosY = hero.position.y + heroOffset.y;
 
     shadow.drawImage(ctx, heroPosX, heroPosY);
     hero.drawImage(ctx, heroPosX, heroPosY);
---
+++ frontend/game_project_react/src/overworldComponents/helpers/grid.js
@@ -0,0 +1,13 @@
+// Locks human movement to a grid.
+
+export const gridCells = (n) => {
+  return n * 16;
+};
+
+export const isSpaceFree = (walls, x, y) => {
+  // Convert to string format for easy lookup.
+  const str = `${x},${y}`;
+  // Check if walls has an entry at this spot.
+  const isWallPresent = walls.has(str);
+  return !isWallPresent;
+};
